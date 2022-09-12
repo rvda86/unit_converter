3d148f7e242dc0fbb7af5df1ded81fe1f40cb242
Robin van den Aardweg <rvda@protonmail.com>
2022-09-12 18:28:32 +0200

    more distance units added

--- src/factory.php
+++ src/factory.php
@@ -9,10 +9,15 @@ require "src/classes/Unit.php";
 require "src/constants.php";
 
 function unitFactory(String $unit) {
-    $units = array("meter"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["meter"]), "distance"), 
+    $units = array(
+                    "millimeter"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["millimeter"]), "distance"), 
                     "centimeter"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["centimeter"]), "distance"), 
+                    "meter"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["meter"]), "distance"), 
+                    "kilometer"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["kilometer"]), "distance"), 
                     "inch"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["inch"]), "distance"),
+                    "foot"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["foot"]), "distance"), 
                     "yard"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["yard"]), "distance"),
+                    "mile"=>new Unit(new DistanceConverter(DISTANCES_RATIO_TO_METER["mile"]), "distance"), 
                     "celcius"=>new Unit(new CelciusConverter(), "temperature"),
                     "kelvin"=>new Unit(new KelvinConverter(), "temperature"),
                     "fahrenheit"=>new Unit(new FahrenheitConverter(), "temperature"),
