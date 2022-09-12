84726d4ac79aae7df0fa6e3836bdc69d35df46df
Robin van den Aardweg <rvda@protonmail.com>
2022-09-12 18:25:45 +0200

    more distance units added

--- src/constants.php
+++ src/constants.php
@@ -1,10 +1,16 @@
 <?php
 
-define("DISTANCES", array("centimeter", "meter", "inch", "yard"));
-define("DISTANCES_RATIO_TO_METER", array("centimeter"=>"0.01", 
+define("DISTANCES", array("millimeter", "centimeter", "meter", "kilometer", "inch", "foot", "yard", "mile"));
+define("DISTANCES_RATIO_TO_METER", array(
+                                        "millimeter"=>"0.001",
+                                        "centimeter"=>"0.01", 
                                         "meter"=>"1",
+                                        "kilometer"=>"1000",
                                         "inch"=>"0.0254",
-                                        "yard"=>"0.9144"));
+                                        "foot"=>"0.3048",
+                                        "yard"=>"0.9144",
+                                        "mile"=>"1609.344"
+                                        ));
 define("TEMPERATURES", array("celcius", "kelvin", "fahrenheit", "rankine"));
 
 ?>
